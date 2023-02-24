4ad8ec309fde6f852a104122bb27b4d9d0cb435c
Rohit Ranjan <rohitgns54321@gmail.com>
2023-02-24 22:56:35 +0530

    RemainderTheorem

---
+++ RemainderTheorem.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class RemainderTheorem {
+    public static void main(String[] args) {
+        int num[] = { 3, 4, 5 };
+        int rem[] = { 2, 3, 1 };
+        int i = 0;
+
+        int x = 1, n = 1000;
+        while (x < n) {
+            for (i = 0; i < rem.length; i++) {
+                if (x % num[i] != rem[i]) {
+                    break;
+                }
+            }
+            if (i == rem.length) {
+                System.out.println(x);
+            }
+            x++;
+        }
+    }
+}
