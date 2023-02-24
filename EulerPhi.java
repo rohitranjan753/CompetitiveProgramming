241579f0a70425b16251183634300b60286971a1
Rohit Ranjan <rohitgns54321@gmail.com>
2023-02-24 19:43:23 +0530

    eulerphi

---
+++ EulerPhi.java
@@ -0,0 +1,22 @@
+class EulerPhi {
+    // Function to return GCD of a and b
+    static int gcd(int a, int b) {
+        if (a == 0)
+            return b;
+        return gcd(b % a, a);
+    }
+
+    static int phi(int n) {
+        int result = 1;
+        for (int i = 2; i < n; i++)
+            if (gcd(i, n) == 1)
+                result++;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int n;
+        for (n = 1; n <= 10; n++)
+            System.out.println("phi(" + n + ") = " + phi(n));
+    }
+}
