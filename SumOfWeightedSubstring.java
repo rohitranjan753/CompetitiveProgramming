3838a73e3a96c71d4efb9d8e2d7fc4d3eff453ff
Rohit Ranjan <rohitgns54321@gmail.com>
2023-04-06 09:52:36 +0530

    sumofweightedsubstring

--- SumOfWeightedSubstring.java
+++ SumOfWeightedSubstring.java
@@ -6,25 +6,34 @@
 // false    break;
 
 import java.util.*;
-class SumOfWeightedSubstring{
-    static void weightedSubstring(String stringValue, int K, int Q){
+
+class SumOfWeightedSubstring {
+    static void weightedSubstring(String stringValue, int K, String Q) {
         char string[] = stringValue.toCharArray();
-        int q[] = Q
-        for(int i = 0; i < string.length(); i++){
-            int sum=0;
-            String s="";
-            for(int j = i; j < string.length(); j++){
-                    int current = string.charAt(j) - 'a';
-                    sum +=Q.charAt(current)-'0';
+        String result = "";
+        for (int i = 0; i < string.length; i++) {
+            int sum = 0;
+            String s = "";
+            for (int j = i; j < string.length; j++) {
+                int currentPos = stringValue.charAt(j) - 'a';
+                sum += Q.charAt(currentPos) - '0';
+                s += stringValue.charAt(j);
+
+                if (sum <= K) {
+                    result = s;
+                } else {
+                    break;
+                }
             }
         }
+        System.out.println("Length of Wieghted Subarray is:" + result.length());
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String originalString = sc.next();
         int K = sc.nextInt();
-        int Q = sc.nextLine();
+        String Q = "12345678912345678912345678";
         weightedSubstring(originalString, K, Q);
 
     }
