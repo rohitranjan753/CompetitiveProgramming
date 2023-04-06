d90a7b86bcf5b7896a5be9c41842bc60008fcc11
Rohit Ranjan <rohitgns54321@gmail.com>
2023-04-06 14:46:45 +0530

    Maneuvering

--- Maneuvering.java
+++ Maneuvering.java
@@ -1,18 +1,27 @@
 // Maneuvering
 
 import java.util.*;
-class Maneuvering{
 
-    static int pathCalculation(int rows, int columns){
-        if(rows == rows-1 || columns == columns-1){
+class Maneuvering {
+    static int pathCalculationReverse(int rows, int columns) {
+        if (rows == 0 || columns == 0) {
             return 1;
         }
-        return pathCalculation(rows, columns+1)+pathCalculation(rows+1, columns);
+        return pathCalculation(rows - 1, columns) + pathCalculation(rows, columns - 1);
     }
+
+    static int pathCalculation(int rows, int columns) {
+        if (rows == rows - 1 || columns == columns - 1) {
+            return 1;
+        }
+        return pathCalculation(rows, columns + 1) + pathCalculation(rows + 1, columns);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int row = scanner.nextInt();
         int col = scanner.nextInt();
-        pathCalculation(row, col);
+        System.out.println(pathCalculation(row, col));
+        System.out.println(pathCalculation(row - 1, col - 1));
     }
 }
