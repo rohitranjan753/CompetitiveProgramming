c72aab58c90ca99bd8b92e0bfa74d09c473b41ac
Rohit Ranjan <rohitgns54321@gmail.com>
2023-07-29 09:48:54 +0530

    RemainerTheorem and complexity text file

--- RemainderTheorem.java
+++ RemainderTheorem.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 class RemainderTheorem {
     public static void main(String[] args) {
-        int num[] = { 25, 23 };
-        int rem[] = { 5, 32 };
-        // int num[] = { 3, 4, 5 };
-        // int rem[] = { 2, 3, 1 };
+        // int num[] = { 25, 23 };
+        // int rem[] = { 5, 32 };
+        int num[] = { 3, 4, 5 };
+        int rem[] = { 2, 3, 1 };
         int i = 0;
 
         int x = 1, n = 3;
